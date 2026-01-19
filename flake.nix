9570a561c45a6159babc343ffc9ca9f1ccde3e7f
nixos <nixos>
2026-01-19 10:14:27 +0800

    [chore] Reformat

--- flake.nix
+++ flake.nix
@@ -4,36 +4,41 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    poetry2nix,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
-  in {
-    packages.${system}.default = let
-      src = pkgs.fetchFromGitHub {
-        owner = "awslabs";
-        repo = "git-remote-s3";
-        rev = "v0.3.1";
-        hash = "sha256-+50yq54ROvN7VA0JTxJ+v1izJigTYt119dqaX+MSODQ=";
-      };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      poetry2nix,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
     in
-      mkPoetryApplication {projectDir = src;};
+    {
+      packages.${system}.default =
+        let
+          src = pkgs.fetchFromGitHub {
+            owner = "awslabs";
+            repo = "git-remote-s3";
+            rev = "v0.3.1";
+            hash = "sha256-+50yq54ROvN7VA0JTxJ+v1izJigTYt119dqaX+MSODQ=";
+          };
+        in
+        mkPoetryApplication { projectDir = src; };
 
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        alejandra
-        nil
-      ];
+      formatter.${system} = pkgs.nixfmt-tree;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          alejandra
+          nil
+        ];
+      };
+
+      nixosConfigurations.hostname = pkgs.lib.nixosSystem {
+        modules = [ ];
+      };
+
+      templates.default.path = ./.;
     };
-
-    nixosConfigurations.hostname = pkgs.lib.nixosSystem {
-      modules = [];
-    };
-
-    templates.default.path = ./.;
-  };
 }
